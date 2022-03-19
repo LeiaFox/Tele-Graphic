f09f4f4e51ddd1675a506b85a1006b0169d5aee1
LeiaFox <50370419+LeiaFox@users.noreply.github.com>
2022-03-19 20:48:00 +0000

    fart

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import time
 import tkinter
 import sys
+import textwrap
+import random
 from tkinter import ttk
 import termcolor
 from termcolor import colored
@@ -11,6 +13,8 @@ from termcolor import colored
 #https://stackoverflow.com/questions/62564224/python-text-rpg-how-to-save-load
 
 new = colored("New Game","red")
+
+screen_width = 100
 
 def clear_screen():
   if sys.platform == 'win32':
@@ -30,5 +34,65 @@ def savegame(save_name):
 
 #first thing people see
 print("Welcome to TeleGraphic, A text-based game about death, murder, and living in a future desensitised to gore and incomprehensible horror.")
-while True:
-	newgame = input(f"\n\n\nWould you like to start a {new}? ")
+
+class player:
+	def __init__(self):
+		self.name = ''
+		self.hp = 0
+		self.stamina = 0
+		self.status_effect = []
+		self.location = ''
+my_player = player()
+
+def title_screen_selections():
+	while True:
+		option = input("> ").lower()
+		if option == "start":
+			start_game() #placeholder
+			break
+		elif option == "help":
+			help_menu() #placeholder
+			break
+		elif option == "quit":
+			exit(0)
+			break
+		else:
+			print("Exception: Invalid command")
+			continue
+
+def title_screen():
+	clear_screen()
+	print('''
+###################################
+# Welcome to TeleGraphic,         #
+# A text-based game about death,  #
+# murder, and living in a future  #
+# desensitised to gore and        #
+# incomprehensible horror.        #
+###################################
+
+				
+            - Play -
+            - Help -
+            - Quit -
+	    Copyright 2022 LeiaFox
+	''')
+	title_screen_selections()
+
+def help_menu():
+	print('''
+###################################
+# Welcome to TeleGraphic,         #
+# A text-based game about death,  #
+# murder, and living in a future  #
+# desensitised to gore and        #
+# incomprehensible horror.        #
+###################################
+
+
+  - Type up, down, left or right to 
+	move around the place
+  - Type your commands to do them
+  - Type "inspect" to inspect something around you
+	
+	''')
