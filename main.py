91debaa7bc03a722cfbba8911a65f61b45572617
Leonardo Martinez-Davydov <leonardo_c_martinez@hotmail.com>
2022-03-19 22:49:25 +0000

    farted sharted asdf

--- main.py
+++ main.py
@@ -9,60 +9,64 @@ from tkinter import ttk
 import termcolor
 from termcolor import colored
 
-#useful info
-#https://stackoverflow.com/questions/62564224/python-text-rpg-how-to-save-load
+# useful info
+# https://stackoverflow.com/questions/62564224/python-text-rpg-how-to-save-load
 
-new = colored("New Game","red")
+new = colored("New Game", "red")
 
 screen_width = 100
 
+
 def clear_screen():
-  if sys.platform == 'win32':
-    os.system('cls')
-  else:
-    os.system('clear')
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-def savegame(save_name):
-	if not os.path.exists(save_name):
-		os.makedirs(save_name)
-		with open(save_name+"/"+save_name+".json", 'w') as f:
-			json.dump(place, f)
-		with open(save_name+"/inventory.json", 'w') as f:
-			json.dump(inventory, f)
-	else:
-		print("Exception: Save file name already exists!")
 
-#first thing people see
-print("Welcome to TeleGraphic, A text-based game about death, murder, and living in a future desensitised to gore and incomprehensible horror.")
+# def save_game(save_name):
+# if not os.path.exists(save_name):
+# os.makedirs(save_name)
+# with open(save_name + "/" + save_name + ".json", 'w') as f:
+# json.dump(place, f)
+# with open(save_name + "/inventory.json", 'w') as f:
+# json.dump(inventory, f)
+# else:
+# print("Exception: Save file name already exists!")
+
 
 class player:
-	def __init__(self):
-		self.name = ''
-		self.hp = 0
-		self.stamina = 0
-		self.status_effect = []
-		self.location = ''
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.stamina = 0
+        self.status_effect = []
+        self.location = ''
+
+
 my_player = player()
 
+
 def title_screen_selections():
-	while True:
-		option = input("> ").lower()
-		if option == "start":
-			start_game() #placeholder
-			break
-		elif option == "help":
-			help_menu() #placeholder
-			break
-		elif option == "quit":
-			exit(0)
-			break
-		else:
-			print("Exception: Invalid command")
-			continue
+    while True:
+        option = input("\n> ").lower()
+        if option == "start":
+            start_game()  # placeholder
+            break
+        elif option == "help":
+            help_menu()  # placeholder
+            break
+        elif option == "quit":
+            exit(0)
+            break
+        else:
+            print("Exception: Invalid command")
+            continue
+
 
 def title_screen():
-	clear_screen()
-	print('''
+    clear_screen()
+    print('''
 ###################################
 # Welcome to TeleGraphic,         #
 # A text-based game about death,  #
@@ -77,10 +81,11 @@ def title_screen():
             - Quit -
 	    Copyright 2022 LeiaFox
 	''')
-	title_screen_selections()
+    title_screen_selections()
+
 
 def help_menu():
-	print('''
+    print('''
 ###################################
 # Welcome to TeleGraphic,         #
 # A text-based game about death,  #
@@ -90,9 +95,41 @@ def help_menu():
 ###################################
 
 
-  - Type up, down, left or right to 
-	move around the place
+  - Type up, down, left or right 
+    to move around the place
   - Type your commands to do them
-  - Type "inspect" to inspect something around you
+  - Type "inspect" to inspect 
+  	something around you
 	
 	''')
+
+
+#### GAME FUNCTIONALITY ####
+# def start_game():
+
+
+#### MAP ####
+
+"""
+a1    a2   a3  a4
+-----------------
+¦   ¦   ¦   ¦   ¦ b1
+-----------------
+¦   ¦   ¦   ¦   ¦ b2
+-----------------
+¦   ¦   ¦   ¦   ¦ b3
+-----------------
+¦   ¦   ¦   ¦   ¦ b4
+-----------------
+"""
+
+DESCRIPTION = 'description'
+EXAMINATION = 'examination'
+COMPLETE = False
+UP = "up", "north"
+DOWN = "down", "south"
+LEFT = "left", "west"
+RIGHT = "right", "east"
+
+while True:
+    title_screen()
